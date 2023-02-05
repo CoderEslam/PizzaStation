202e03f8bb12ee5a91d50fcc7e95e51f0d79ab09
CoderEslam <eslamghazy600@gmail.com>
2023-02-05 09:07:55 +0200

    PizzaStation

---
+++ androidApp/src/main/java/com/doubleclick/pizzastation/android/Adapter/CustomPizzaItemAdapter.kt
@@ -0,0 +1,49 @@
+package com.doubleclick.pizzastation.android.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.doubleclick.pizzastation.android.R
+import com.doubleclick.pizzastation.android.model.MenuModel
+import com.doubleclick.pizzastation.android.utils.Constants.IMAGE_URL
+import de.hdodenhof.circleimageview.CircleImageView
+
+class CustomPizzaItemAdapter(val menus: ArrayList<MenuModel>) :
+    RecyclerView.Adapter<CustomPizzaItemAdapter.CustomPizzaItemViewHolder>() {
+
+    private lateinit var onPizzaClicked: onPizzaClicked
+
+    fun callBack(onPizzaClicked: onPizzaClicked) {
+        this.onPizzaClicked = onPizzaClicked
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomPizzaItemViewHolder {
+        return CustomPizzaItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_custom_pizza, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: CustomPizzaItemViewHolder, position: Int) {
+        holder.name.text = menus[position].name
+        Glide.with(holder.itemView.context).load(IMAGE_URL + menus[position]).into(holder.image)
+        holder.itemView.setOnClickListener {
+            onPizzaClicked.onPizza(menus[position])
+        }
+    }
+
+    override fun getItemCount(): Int = menus.size
+
+    inner class CustomPizzaItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: CircleImageView = itemView.findViewById(R.id.image);
+        val name: TextView = itemView.findViewById(R.id.name);
+    }
+
+
+}
+
+interface onPizzaClicked {
+    fun onPizza(menu: MenuModel)
+}
--- androidApp/src/main/java/com/doubleclick/pizzastation/android/CustomizePizzaActivity.kt
+++ androidApp/src/main/java/com/doubleclick/pizzastation/android/CustomizePizzaActivity.kt
@@ -1,69 +1,90 @@
 package com.doubleclick.pizzastation.android
 
-import android.content.Intent
-import android.net.Uri
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.doubleclick.pizzastation.android.Adapter.CustomPizzaItemAdapter
+import com.doubleclick.pizzastation.android.Adapter.onPizzaClicked
+import com.doubleclick.pizzastation.android.Repository.remot.RepositoryRemot
+import com.doubleclick.pizzastation.android.ViewModel.MainViewModel
+import com.doubleclick.pizzastation.android.ViewModel.MainViewModelFactory
 import com.doubleclick.pizzastation.android.databinding.ActivityCustomizePizzaBinding
+import com.doubleclick.pizzastation.android.model.MenuList
+import com.doubleclick.pizzastation.android.model.MenuModel
+import com.doubleclick.pizzastation.android.utils.Constants.IMAGE_URL
+import kotlinx.android.synthetic.main.activity_customize_pizza.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class CustomizePizzaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCustomizePizzaBinding
-    private var uri1: Uri? = null;
-    private var uri2: Uri? = null;
-    private var uri3: Uri? = null;
-    private var uri4: Uri? = null;
+    private lateinit var viewModel: MainViewModel
+    private val TAG = "CustomizePizzaActivity"
+    private var menus: ArrayList<MenuModel> = ArrayList();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomizePizzaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val viewModelFactory = MainViewModelFactory(RepositoryRemot())
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
+        viewModel.getMenu().observe(this) {
+            it.enqueue(object : Callback<MenuList> {
+                override fun onResponse(
+                    call: Call<MenuList>,
+                    response: Response<MenuList>
+                ) {
+                    menus.addAll(response.body()!!.data)
+                }
 
+                override fun onFailure(call: Call<MenuList>, t: Throwable) {
+                    Log.e(TAG, "onFailure: ${t.message}")
+                }
+
+            })
+        }
         binding.quarter1.setOnClickListener {
-            binding.quarter1.setImageDrawable(resources.getDrawable(R.drawable.pizza))
+            selectQurter(quarter1)
         }
         binding.quarter2.setOnClickListener {
-            binding.quarter2.setImageDrawable(resources.getDrawable(R.drawable.bg))
+            selectQurter(quarter2)
         }
         binding.quarter3.setOnClickListener {
-            binding.quarter3.setImageDrawable(resources.getDrawable(R.drawable.heart))
+            selectQurter(quarter3)
         }
         binding.quarter4.setOnClickListener {
-            binding.quarter4.setImageDrawable(resources.getDrawable(R.drawable.bg))
+            selectQurter(quarter4)
         }
 
     }
 
-    fun openGellry() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(intent, 1000)
+    private fun selectQurter(image: ImageView) {
+        val builder = AlertDialog.Builder(this@CustomizePizzaActivity);
+        builder.setTitle("select")
+        val view: View = LayoutInflater.from(this@CustomizePizzaActivity)
+            .inflate(R.layout.rv_custom_pizza, null, false)
+        val items: RecyclerView = view.findViewById(R.id.rv_pizza);
+        val adapter = CustomPizzaItemAdapter(menus)
+        items.adapter = adapter
+        adapter.callBack(object : onPizzaClicked {
+            override fun onPizza(menu: MenuModel) {
+                Glide.with(this@CustomizePizzaActivity).load(IMAGE_URL + menu.image).into(image)
+            }
+        })
+        builder.setCancelable(true);
+        builder.setView(view);
+        builder.show()
     }
 
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1000 && resultCode == RESULT_OK && data != null) {
-            if (uri1 == null) {
-                uri1 = data.data!!
-                binding.quarter1.setImageURI(uri1)
-            }
-            if (uri2 == null) {
-                uri2 = data.data!!
-                binding.quarter2.setImageURI(uri2)
-            }
-            if (uri3 == null) {
-                uri3 = data.data!!
-                binding.quarter3.setImageURI(uri3)
-            }
-            if (uri4 == null) {
-                uri4 = data.data!!
-                binding.quarter4.setImageURI(uri4)
-            }
-
-        }
-    }
 
 }
